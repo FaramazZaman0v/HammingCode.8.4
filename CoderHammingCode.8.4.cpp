e8c632b3aab714f24a1e1248daeb0f8f85f0af02
FaramazZaman0v <faramaz.zamanov@gmail.com>
2024-05-04 00:05:52 +0400

    Update CoderHammingCode.8.4.cpp

--- CoderHammingCode.8.4.cpp
+++ CoderHammingCode.8.4.cpp
@@ -1,68 +1,80 @@
 #include <iostream>
-#include <vector>
+#include <bitset>
+
+std::bitset<16> coder(int a){
+	std::bitset<8> d=a, p;
+	std::bitset<16> C;
+	
+	p[0]=((1+d[0]+d[2]+d[3])%2);
+	p[1]=((1+d[0]+d[1]+d[3])%2);
+	p[2]=((1+d[0]+d[1]+d[2])%2);
+	p[3]=((1+p[0]+d[0]+p[1]+d[1]+p[2]+d[2]+d[3])%2); 
+	if((d[0]+d[2]+d[3]+p[0])%2!=1) return -1;
+
+	p[4] = (1 + d[4] + d[6] + d[7]) % 2;
+	p[5] = (1 + d[4] + d[5] + d[7]) % 2;
+	p[6] = (1 + d[4] + d[5] + d[6]) % 2;
+	p[7] = (1 + p[4] + d[4] + p[5] + d[5] + p[6] + d[6] + d[7]) % 2;
+	if((d[4]+d[6]+d[7]+p[4])%2!=1) return -2;
+
+	for(int i=0; i<8; i++){
+		C[2*i]=d[i];
+		C[2*i+1]=p[i];
+	}
+	return C;
+}
+
+
+std::bitset<8> decoder(std::bitset<16> R){
+	std::bitset<8> Dec;
+	bool A=R[0]^R[2]^R[4]^R[6] ;
+	bool B=R[1]^R[2]^R[5]^R[6] ;
+	bool C=R[3]^R[4]^R[5]^R[6] ;
+	bool D=R[0]^R[1]^R[2]^R[3]^R[4]^R[5]^R[6]^R[7] ;
+	
+	int P=(C*4)+(B*2)+A-1;
+	
+	if( !A && !B && !C && !D ) {
+		std::cout << "No errors\n" ;
+	} else if ( D && P==-1) {
+		std::cout << "Error at parity bit\n" ;
+	} else if( !D && A+B+C!=0 ) {
+		std::cout << "Double error - Packet rejected\n" ;
+		return -3;
+	}else if( D && A+B+C!=0 ) {
+		std::cout << "Error at position: " << P << "\n" ;
+		R.flip(P) ;
+	} else {
+		std::cout << "Unknown error. Packet rejected\n" ;
+		return -4;
+	}
+	 	
+	for(int i=0; i<8; i++){
+		Dec[i]=R[2*i];
+	}
+
+	return Dec;
+}
+
 
 int main(){
-	std::vector<int> d, p;
-	int U=0, tmp1, tmp2=0, c=0;
-//	char C;
-//	std::cin >> C;
+	int U=0, tmp=0;
+	std::bitset<16> C;
+	std::bitset<8> Dec;
 	
 	std::cin >> U;
-	if(U<0 || U>255) return -3;
+	if(U<0 || U>255) return -5;
 	
-	tmp1=U;
+	C=coder(U);
+	std::cout << C << "\n";
 	
-	for(int i=0; i<4; i++){
-		c=tmp1&1;
-		if(c>0) d.push_back(1);
-		else d.push_back(0);
-		tmp1=tmp1>>1;
+	Dec=decoder(C);
+	std::cout << Dec << "\n";
+	
+	for(int i=1; i<9; i++){
+		tmp<<=1;
+		tmp|=Dec[8-i];
 	}
-	
-	p.push_back((1+d[0]+d[2]+d[3])%2);
-	p.push_back((1+d[0]+d[1]+d[3])%2);
-	p.push_back((1+d[0]+d[1]+d[2])%2);
-	p.push_back((1+p[0]+d[0]+p[1]+d[1]+p[2]+d[2]+d[3])%2);
-	
-	if((d[0]+d[2]+d[3]+p[0])%2!=1) return -1;
-	
-	tmp1=0;
-	for(int i=0; i<(int)d.size(); i++){
-		tmp1=tmp1|p[i];
-		tmp1=tmp1<<1;
-		tmp1=tmp1|d[i];
-		tmp1=tmp1<<1;
-	}
-//----------------------------------------------------------------------
-	
-	d.clear();
-	p.clear();
-	tmp2=U>>4;
-	for(int i=0; i<4; i++){
-		c=tmp2&1;
-		if(c>0) d.push_back(1);
-		else d.push_back(0);
-		tmp2=tmp2>>1;
-	}
-	
-	p.push_back((1+d[0]+d[2]+d[3])%2);
-	p.push_back((1+d[0]+d[1]+d[3])%2);
-	p.push_back((1+d[0]+d[1]+d[2])%2);
-	p.push_back((1+p[0]+d[0]+p[1]+d[1]+p[2]+d[2]+d[3])%2);
-	
-	if((d[0]+d[2]+d[3]+p[0])%2!=1) return -2;
-	
-	tmp2=0;
-	for(int i=0; i<(int)d.size(); i++){
-		tmp2=tmp2|p[i];
-		tmp2=tmp2<<1;
-		tmp2=tmp2|d[i];
-		tmp2=tmp2<<1;
-	}
-//----------------------------------------------------------------------
-
-	tmp2=tmp2<<4;
-	tmp1=tmp1|tmp2;
-	std::cout << tmp1 << "\n";
+	std::cout << tmp << "\n";
 	return 0;
 }
